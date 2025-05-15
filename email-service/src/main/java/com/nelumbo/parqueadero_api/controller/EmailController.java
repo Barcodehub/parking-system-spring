ba19fb274a77cd26ed530ccba94019214d416784
Barcodehub <alex.barco.maicol@gmail.com>
2025-05-15 12:40:12 -0500

    valid-email

--- email-service/src/main/java/com/nelumbo/parqueadero_api/controller/EmailController.java
+++ email-service/src/main/java/com/nelumbo/parqueadero_api/controller/EmailController.java
@@ -2,10 +2,14 @@ package com.nelumbo.parqueadero_api.controller;
 
 import com.nelumbo.parqueadero_api.dto.EmailRequest;
 import com.nelumbo.parqueadero_api.dto.EmailResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -13,7 +17,7 @@ import java.time.LocalDateTime;
 public class EmailController {
 
     @PostMapping("/send-email")
-    public ResponseEntity<EmailResponse> sendEmail(@RequestBody EmailRequest request) {
+    public ResponseEntity<EmailResponse> sendEmail(@Valid @RequestBody EmailRequest request) {
         // Simulación de envío de correo
         System.out.println("Simulando envío de email a: " + request.getEmail());
         System.out.println("Detalles: " + request.getMessage());
@@ -24,4 +28,24 @@ public class EmailController {
 
         return ResponseEntity.ok(response);
     }
+
+
+
+
+
+    @RestControllerAdvice
+    public class GlobalExceptionHandler {
+
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+        public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+            Map<String, String> errors = new HashMap<>();
+
+            ex.getBindingResult().getFieldErrors().forEach(error -> {
+                errors.put(error.getField(), error.getDefaultMessage());
+            });
+
+            return ResponseEntity.badRequest().body(errors);
+        }
+    }
+
 }
--- email-service/src/main/java/com/nelumbo/parqueadero_api/dto/EmailRequest.java
+++ email-service/src/main/java/com/nelumbo/parqueadero_api/dto/EmailRequest.java
@@ -1,5 +1,8 @@
 package com.nelumbo.parqueadero_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailRequest {
+    @Email(message = "Formato de correo inválido")
     private String email;
+    @Size(min = 6, max = 6, message = "La placa debe tener 6 caracteres")
     private String placa;
+    @NotBlank(message = "El campo mensaje no puede estar vacío")
     private String message;
     private Integer parqueaderoId;
 }
