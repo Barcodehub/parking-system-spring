2823634102c99bfb866eb8e1e1b6a373b8f03227
Barcodehub <alex.barco.maicol@gmail.com>
2025-07-01 09:18:15 -0500

    Cola de emails Rabbith y dlq

--- email-service/src/main/java/com/nelumbo/parqueadero_api/config/EmailConsumer.java
+++ email-service/src/main/java/com/nelumbo/parqueadero_api/config/EmailConsumer.java
@@ -1,7 +1,16 @@
 package com.nelumbo.parqueadero_api.config;
 
 import com.nelumbo.parqueadero_api.dto.BulletinEmailDTO;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
@@ -14,10 +23,15 @@ public class EmailConsumer {
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE, concurrency = "2")
     public void processBulletin(BulletinEmailDTO emailData) throws InterruptedException {
+
+        if (emailData.getEmail().equals("juan2@example.com")) {
+            throw new RuntimeException("Error simulado en el consumidor para " + emailData.getEmail());
+        }
         logger.info("Procesando correo a: " + emailData.getEmail());
 
         Thread.sleep(new Random().nextInt(4000) + 1000); // Simula envío entre 1 y 5 segundos
 
         logger.info("Correo enviado exitosamente a: " + emailData.getEmail());
     }
+
 }
--- email-service/src/main/java/com/nelumbo/parqueadero_api/config/RabbitMQConfig.java
+++ email-service/src/main/java/com/nelumbo/parqueadero_api/config/RabbitMQConfig.java
@@ -1,12 +1,17 @@
 package com.nelumbo.parqueadero_api.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.retry.interceptor.RetryOperationsInterceptor;
 
 @Configuration
 public class RabbitMQConfig {
@@ -15,8 +20,17 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue emailQueue() {
-        return new Queue(QUEUE, false);
+        return QueueBuilder.durable(QUEUE)
+                .withArgument("x-dead-letter-exchange", "")
+                .withArgument("x-dead-letter-routing-key", QUEUE + ".dlq")
+                .build();
     }
+
+    @Bean
+    public Queue emailQueueDlq() {
+        return QueueBuilder.durable(QUEUE + ".dlq").build();
+    }
+
 
     @Bean
     public MessageConverter jsonMessageConverter() {
@@ -28,5 +42,24 @@ public class RabbitMQConfig {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter);
         return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory,
+                                                                               MessageConverter messageConverter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(messageConverter);
+
+        // Retry con 3 intentos, backoff exponencial
+        RetryOperationsInterceptor retryInterceptor = RetryInterceptorBuilder.stateless()
+                .maxAttempts(3) // Máximo 3 intentos
+                .backOffOptions(1000, 2.0, 10000) // 1s, 2s, 4s...
+                .recoverer(new RejectAndDontRequeueRecoverer()) //  mandar a DLQ
+                .build();
+
+        factory.setAdviceChain(retryInterceptor);
+        factory.setDefaultRequeueRejected(false); // No volver a la cola normal
+        return factory;
     }
 }
